6c5bbe55882e5a60533ef7266acc436b12af76dc
Hyeong Jin <gudwls9280@naver.com>
2026-05-26 14:55:59 +0900

    fix(payment): 머지 충돌 해소 시 누락된 package 선언 복원

--- services/payment-service/src/main/java/com/bank/payment/PaymentServiceApplication.java
+++ services/payment-service/src/main/java/com/bank/payment/PaymentServiceApplication.java
@@ -1,3 +1,5 @@
+package com.bank.payment;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
