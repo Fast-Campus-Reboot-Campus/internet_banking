9e21a6bbe8efe4ed62c353ba39b1919771217a74
정혜영 <greenenjoyment@gmail.com>
2026-05-27 12:36:30 +0900

    fix(deposit): 원격 병합 후 설정 충돌 정리

--- services/deposit-service/src/main/java/com/bank/deposit/DepositServiceApplication.java
+++ services/deposit-service/src/main/java/com/bank/deposit/DepositServiceApplication.java
@@ -2,12 +2,8 @@ package com.bank.deposit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = {"com.bank.deposit", "com.bank.common"})
-@EntityScan(basePackages = {"com.bank.deposit", "com.bank.common"})
-@EnableJpaRepositories(basePackages = {"com.bank.deposit", "com.bank.common"})
+@SpringBootApplication
 public class DepositServiceApplication {
 
     public static void main(String[] args) {
