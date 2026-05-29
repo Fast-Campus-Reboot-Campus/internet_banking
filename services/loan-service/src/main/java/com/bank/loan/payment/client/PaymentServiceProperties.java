13a9eb0dc73977f0cd15d280a350ba67afc9481d
hyemin_yang <asitwas729@gmail.com>
2026-05-29 10:34:39 +0900

    feat(loan): 대출실행 payment disbursement 계좌 설정 추가

--- services/loan-service/src/main/java/com/bank/loan/payment/client/PaymentServiceProperties.java
+++ services/loan-service/src/main/java/com/bank/loan/payment/client/PaymentServiceProperties.java
@@ -6,11 +6,16 @@ import org.springframework.boot.context.properties.bind.DefaultValue;
 @ConfigurationProperties(prefix = "payment")
 public record PaymentServiceProperties(
         @DefaultValue("http://localhost:8080") String url,
-        Collection collection
+        Collection collection,
+        Disbursement disbursement
 ) {
     public record Collection(
             @DefaultValue("088") String bankCode,
             @DefaultValue("00000000000") String accountNo,
             @DefaultValue("한국은행") String holderName
+    ) {}
+
+    public record Disbursement(
+            @DefaultValue("BANK_DISBURSE_001") String accountId
     ) {}
 }
